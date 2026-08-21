39c7ee05e72bb6e0b343a1cf5a6d1489c4b27ca5
Saurabh Verma <vermaonto181003@gmail.com>
2026-08-21 10:36:20 +0530

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -9,6 +9,7 @@ log_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(log_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
+# print(LOG_FILE_PATH)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
